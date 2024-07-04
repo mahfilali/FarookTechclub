8e6e713ea8a6de6b1338b10f4bd1198c4cc96061
Mahfil Ali <mahfilali20080@gmail.com>
2024-07-04 20:33:15 +0530

    Initial

--- js/fr.js
+++ js/fr.js
@@ -14,3 +14,14 @@ window.addEventListener("load", (event)=>{
         showHideMenu()
     });
 });
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        autoWidth:true,
+        margin:10,
+    });
+});
